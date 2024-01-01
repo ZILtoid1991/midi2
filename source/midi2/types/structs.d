8ae8028802b8a210aefa2210f5209c56edc6e546
ZILtoid1991 <laszloszeremi@outlook.com>
2024-01-01 02:10:06 +0100

    Fixing the endian issue

--- source/midi2/types/structs.d
+++ source/midi2/types/structs.d
@@ -28,19 +28,30 @@ struct UMP {
 		 * Creates a UMP with two 8 bit fields.
 		 */
 		this(ubyte msgType, ubyte group, ubyte status, ubyte channel, ubyte val0 = 0, ubyte val1 = 0) {
-			bytes[0] = cast(ubyte)((msgType<<4) | (group & 0xF));
-			bytes[1] = cast(ubyte)((status<<4) | (channel & 0xF));
-			bytes[2] = val0;
-			bytes[3] = val1;
+			bytes[3] = cast(ubyte)((msgType<<4) | (group & 0xF));
+			bytes[2] = cast(ubyte)((status<<4) | (channel & 0xF));
+			bytes[1] = val0;
+			bytes[0] = val1;
 		}
 		/**
 		 * Creates a MIDI 1.0 compatible pitch-bend command.
 		 */
 		this(ubyte group, ubyte channel, ushort val) {
-			bytes[0] = (MessageType.MIDI1<<4) | (group & 0xF);
-			bytes[1] = (MIDI1_0Cmd.PitchBend<<4) | (channel & 0xF);
-			bytes[3] = cast(ubyte)((val>>7)&0x7F);
-			bytes[2] = cast(ubyte)(val&0x7F);
+			bytes[3] = (MessageType.MIDI1<<4) | (group & 0xF);
+			bytes[2] = (MIDI1_0Cmd.PitchBend<<4) | (channel & 0xF);
+			bytes[1] = cast(ubyte)((val>>7)&0x7F);
+			bytes[0] = cast(ubyte)(val&0x7F);
+		}
+		///Creates a UMP from a 32 bit integer.
+		this(uint base) {
+			this.base = base;
+		}
+		///Creates a UMP from 4 separate bytes, keeping the correct endian order.
+		this(ubyte[4] bytes) {
+			this.bytes[0] = bytes[3];
+			this.bytes[1] = bytes[2];
+			this.bytes[2] = bytes[1];
+			this.bytes[3] = bytes[0];
 		}
 		/**
 		 * Returns the message type of the packet.
@@ -48,21 +59,21 @@ struct UMP {
 		 * This value should not be changed on the fly.
 		 */
 		ubyte msgType() const {
-			return bytes[0]>>4;
+			return bytes[3]>>4;
 		}
 		/**
 		 * Returns the group of the packet. 
 		 */
 		ubyte group() const {
-			return bytes[0] & 0xF;
+			return bytes[3] & 0xF;
 		}
 		/**
 		 * Sets the group of the packet.
 		 */
 		ubyte group(ubyte val) {
-			bytes[0] &= 0xF0;
-			bytes[0] |= val & 0xF;
-			return bytes[0] & 0xF;
+			bytes[3] &= 0xF0;
+			bytes[3] |= val & 0xF;
+			return bytes[3] & 0xF;
 		}
 		/**
 		 * Returns the status value of the field, or 0 if message type hasn't defined it.
@@ -70,22 +81,22 @@ struct UMP {
 		ubyte status() const {
 			switch (msgType) {
 				case MessageType.MIDI2 , MessageType.MIDI1 , MessageType.Data64 , MessageType.Data128:
-					return bytes[1]>>4;
+					return bytes[2]>>4;
 				default:
-					return bytes[1];
+					return bytes[2];
 			}
 		}
 		/**
 		 * Returns the note number of this packet.
 		 */
 		ubyte note() const {
-			return bytes[2];
+			return bytes[1];
 		}
 		/**
 		 * Sets the note number of this packet.
 		 */
 		ubyte note(ubyte val) {
-			return bytes[2] = val;
+			return bytes[1] = val;
 		}
 		alias index = note;
 		alias program = note;
@@ -93,34 +104,39 @@ struct UMP {
 		 * Returns the value of this packet.
 		 */
 		ubyte value() const {
-			return bytes[3];
+			return bytes[0];
 		}
 		/**
 		 * Sets the value of this packet.
 		 */
 		ubyte value(ubyte val) {
-			return bytes[3] = val;
+			return bytes[0] = val;
 		}
 		alias velocity = value;
 		/**
 		 * Returns the pitch bend value of this packet.
 		 */
 		ushort bend() const {
-			return (cast(ushort)bytes[3])<<7 | bytes[2];
+			return (cast(ushort)bytes[0])<<7 | bytes[1];
 		}
 		/**
 		 * Return the channel number of this packet.
 		 */
 		ubyte channel() const {
-			return bytes[1] & 0xF;
+			return bytes[2] & 0xF;
 		}
 		/**
 		 * Sets the channel number of this packet.
 		 */
 		ubyte channel(ubyte val) {
-			bytes[1] &= 0xF0;
-			bytes[1] |= val & 0xF;
-			return bytes[1] & 0xF;
+			bytes[2] &= 0xF0;
+			bytes[2] |= val & 0xF;
+			return bytes[2] & 0xF;
+		}
+		T opCast(T)() const {
+			static if (is(T == uint)) {
+				return base;
+			}
 		}
 	}
 }
@@ -128,8 +144,8 @@ struct UMP {
  * Defines the MIDI 2.0 note commands' data fields.
  */
 struct NoteVals {
-	ushort		velocity;	///Velocity of the note
 	ushort		attrData;	///Attribute data
+	ushort		velocity;	///Velocity of the note
 }
 
 
